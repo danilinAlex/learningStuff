ca3cc62765930fe6ca16fd06ba9aedf1614433ed
Alex Danilin <alex.danilin.ad@gmail.com>
2017-11-01 11:49:36 +0300

    Added base digits

--- iOS/CS193P 2017/Calculator/Calculator/ViewController.swift
+++ iOS/CS193P 2017/Calculator/Calculator/ViewController.swift
@@ -10,8 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var userIsInTheMiddleOfTyping = false
+    var displayValue: Double {
+        get { return Double(display.text!)! }
+        set { display.text = String(newValue) }
+    }
     
+    @IBOutlet weak var display: UILabel!
     
-
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+    }
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            switch mathematicalSymbol {
+                case "π": displayValue = Double.pi
+                case "√": displayValue = sqrt(displayValue)
+                default: break
+            }
+        }
+    }
 }
-
